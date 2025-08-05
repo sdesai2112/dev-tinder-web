357130f8e29413c714931327d11d8171ec467c68
Shraddha <sshradhadesai@gmail.com>
2025-08-05 11:35:46 +0530

    Changing to get profile details and handled navigation with that

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./components/Login";
 import Profile from "./components/Profile";
 import appStore from "./utils/appStore";
 import { Provider } from "react-redux";
+import Feed from "./components/Feed";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <BrowserRouter basename="/">
           <Routes>
             <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />}></Route>
               <Route path="/login" element={<Login />}></Route>
               <Route path="/profile" element={<Profile />}></Route>
             </Route>
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,37 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import { BASE_URL } from "../utils/constants";
+import { addUser } from "../utils/slices/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((store) => store.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const getUser = async () => {
+    if (user) return;
+    try {
+      const user = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+
+      dispatch(addUser(user?.data));
+    } catch (err) {
+      if (err?.status === 401) {
+        navigate("/login");
+      }
+      console.error("Error fetching user data:", err);
+    }
+  };
+
   return (
     <div>
       <NavBar />
